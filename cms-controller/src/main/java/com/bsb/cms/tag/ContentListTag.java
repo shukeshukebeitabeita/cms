5b502e81aa90c0bacb2afcd3366a914b87ac77a3
liuhongjian <51mmei@163.com>
2015-04-26 15:07:50 +0800

    u

--- cms-controller/src/main/java/com/bsb/cms/tag/ContentListTag.java
+++ cms-controller/src/main/java/com/bsb/cms/tag/ContentListTag.java
@@ -1,13 +1,18 @@
 package com.bsb.cms.tag;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
 
 import com.bsb.cms.commons.web.SpringContextUtil;
 import com.bsb.cms.content.service.content.ContContentService;
+import com.bsb.cms.content.service.content.ContTypeCacheService;
+import com.bsb.cms.model.dto.content.ContContentDTO;
+import com.bsb.cms.model.dto.content.ContTypeDTO;
 import com.bsb.cms.model.vo.content.TagContentVO;
 
 import freemarker.core.Environment;
@@ -18,36 +23,66 @@ import freemarker.template.TemplateException;
 import freemarker.template.TemplateModel;
 
 /**
- * 获取多条内容标签。 <cms:clist type_id="1,2", sort="[{id,asc},{name,desc}]" sql=""
- * count=10></cms:clist>
+ * 获取多条内容标签。 <@cms.clist type_id="1,2", sort="[{id,asc},{name,desc}]" sql=""
+ * count=10 hasHot=1 hasLatest=0></@cms.clist>
  * 
  * @author liuhj
  * 
  */
+@Component("contentListTag")
 public class ContentListTag implements TemplateDirectiveModel {
 	private static final Log log = LogFactory.getLog(ContentListTag.class);
-	private ContContentService ContContentService;
+	private ContContentService contContentService;
+	private ContTypeCacheService contTypeCacheService;
 
+	/**
+	 * <@clist type_id="2" count=10 var="cts">
+      <#if (cts?exists && cts?size >0)>
+     <#list cts as c>
+     ${c.title}
+     </#list>
+     </#if>
+     </@clist>
+	 */
 	@Override
 	public void execute(Environment env, Map params, TemplateModel[] loopVars,
 			TemplateDirectiveBody body) throws TemplateException, IOException {
+		Long typeId = null;
+		ContTypeDTO contTypeDTO;
+		List<ContContentDTO> contentList;
+		String varName = "contents";
 		try {
 			if (body != null) {
-				TagContentVO tag;
-				TemplateModel type_id = (TemplateModel) params.get("type_id");
-
-				Long typeId;
-				if (type_id != null) {
-					typeId = Long.valueOf(type_id.toString());
+				TagContentVO conditions;
+				conditions = new TagContentVO();
+				TemplateModel typeIdModel = (TemplateModel) params.get("type_id");
+				TemplateModel countModel = (TemplateModel) params.get("count");
+				TemplateModel varNameModel = (TemplateModel) params.get("var");
+				TemplateModel hasHotModel = (TemplateModel) params.get("hasHot");
+				TemplateModel hasLatestModel = (TemplateModel) params.get("hasLatest");
+				
+				if (varNameModel != null) {
+					varName = varNameModel.toString();
 				}
+				if (countModel != null) {
+					conditions.setCount((Integer.valueOf(countModel.toString())));
+				}
+				if (typeIdModel != null) {
+					typeId = Long.valueOf(typeIdModel.toString());
+					conditions.setType_id(typeId);
+				}
+				if (hasHotModel != null) {
+					conditions.setHasHot(Short.valueOf(hasHotModel.toString()));
+				}
+				if (hasLatestModel != null) {
+					conditions.setHasLatest(Short.valueOf(hasLatestModel.toString()));
+				}
+				
+				contTypeDTO = getContTypeCacheService().getById(typeId);
+				conditions.setLevel(contTypeDTO.getDepth());
+				contentList = getContContentService().findListOfTag(conditions);
 
-				tag = new TagContentVO();
-				tag.setType_id(typeId);
-				tag.setLevel(level);
-				QuanNewestMemberDTO quanNewestMemberDTO = getQuanHeaderInfo(
-						quanIdLong, quanGroup.getMasterId());
-				setVariable(env, "quanMasterInfo", quanNewestMemberDTO);
-
+				setVariable(env, varName, contentList);
 				body.render(env.getOut());
 			} else {
 				throw new TemplateException("clist标签解析错误", env);
@@ -71,12 +106,22 @@ public class ContentListTag implements TemplateDirectiveModel {
 	 * @return
 	 */
 	private ContContentService getContContentService() {
-		if (ContContentService == null) {
-			ContContentService = SpringContextUtil
+		if (contContentService == null) {
+			contContentService = SpringContextUtil
 					.getApplicationContextInstance().getBean(
 							"contContentService", ContContentService.class);
 		}
 
-		return ContContentService;
+		return contContentService;
+	}
+
+	private ContTypeCacheService getContTypeCacheService() {
+		if (contTypeCacheService == null) {
+			contTypeCacheService = SpringContextUtil
+					.getApplicationContextInstance().getBean(
+							"contTypeCacheService", ContTypeCacheService.class);
+		}
+
+		return contTypeCacheService;
 	}
 }
--- cms-model/src/main/java/com/bsb/cms/model/vo/content/TagContentVO.java
+++ cms-model/src/main/java/com/bsb/cms/model/vo/content/TagContentVO.java
@@ -3,56 +3,87 @@ package com.bsb.cms.model.vo.content;
 import com.bsb.cms.model.enums.ContentEnum;
 
 /**
- * 查询栏目下内容标签的条件对象。
- * <cms:clist type_id="1,2", sort="[{id,asc},{name,desc}]" sql="" count=10></cms:clist>
+ * 查询栏目下内容标签的条件对象。 <cms:clist type_id="1,2", sort="[{id,asc},{name,desc}]"
+ * sql="" count=10></cms:clist>
+ * 
  * @author liuhj
- *
+ * 
  */
 public class TagContentVO {
-//	private Long content_id;//取得一条内容；暂时不实现；以后需要单独建一个cms:c标签
+	// private Long content_id;//取得一条内容；暂时不实现；以后需要单独建一个cms:c标签
 	private Long type_id;
-	private int level;//depth
+	private int level;// depth
 	private Integer status = ContentEnum.DEPLOY.getCode();
-	private String sort;//json array eg: [{id,asc},{name,desc}]
-	private String sql;//次参赛有值时，其他参数无效。
-	private String count;//取得条数
+	private String sort;// json array eg: [{id,asc},{name,desc}]
+	private String sql;// 次参赛有值时，其他参数无效。
+	private int count = 10;// 取得条数
+
+	private Short hasHot;
+	private Short hasLatest;
 	
+
+	public Short getHasHot() {
+		return hasHot;
+	}
+
+	public void setHasHot(Short hasHot) {
+		this.hasHot = hasHot;
+	}
+
+	public Short getHasLatest() {
+		return hasLatest;
+	}
+
+	public void setHasLatest(Short hasLatest) {
+		this.hasLatest = hasLatest;
+	}
+
 	public Long getType_id() {
 		return type_id;
 	}
+
 	public void setType_id(Long type_id) {
 		this.type_id = type_id;
 	}
+
 	public int getLevel() {
 		return level;
 	}
+
 	public void setLevel(int level) {
 		this.level = level;
 	}
+
 	public Integer getStatus() {
 		return status;
 	}
+
 	public void setStatus(Integer status) {
 		this.status = status;
 	}
+
 	public String getSort() {
 		return sort;
 	}
+
 	public void setSort(String sort) {
 		this.sort = sort;
 	}
+
 	public String getSql() {
 		return sql;
 	}
+
 	public void setSql(String sql) {
 		this.sql = sql;
 	}
-	public String getCount() {
+
+	public int getCount() {
 		return count;
 	}
-	public void setCount(String count) {
+
+	public void setCount(int count) {
 		this.count = count;
 	}
-	
-	
+
 }
